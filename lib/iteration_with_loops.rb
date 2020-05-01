edf7b708fa705c13e1237e7d7fff5281d806ce03
stephen pesce <stevepesce879@gmail.com>
2020-05-01 20:52:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,18 +2,16 @@ def find_min_in_nested_arrays(src)
   minimums = []
   
   #iterate through days
-  src.each  
-  {|x|
+  src.each{|x|
     min = 999 #new minimum after each day is iterated 
     #iterate through temps in day 
-    x.each  
-    {|y|
+    x.each{|y|
       if y < min 
         min = y   #assign any lesser value to new min 
       end
     }
     minimums.push(min)  #push new min to new array before next day iteration 
   }
-  
+  minumums
 
 end
